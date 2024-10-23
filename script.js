f825407a6aad7d0423066136417102a3ce57c2f6
Alexia Cervantes <Alexiacervantes120@gmail.com>
2024-10-23 14:32:16 -0700

    css & js updates (not working)

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-//this is a comment
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
+
+search.addEventListener('click', () => {
+    const APIKey = '4dfc3be36f00efabce33eedef2c7b79a';
+    const city = document.querySelector('.search-box input').value;
+
+    if(city == '')
+       return;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=standard&appid=${APIKey}`)
+    .then(response => response.json())
+    .then(json => {
+        const image = document.querySelector('.weather-box img');
+        const temperature = document.querySelector('.weather-box .temperature');
+        const description = document.querySelector('.weather-box .description');
+        const humidity = document.querySelector('.weather-details .humidity span');
+        const wind = document.querySelector('.weather-details .wind span');
+
+        switch (json.weather[0].main) {
+            case 'Clear':
+                image.src = 'images/clear.png';
+                break;
+            case 'Rain':
+                image.src = 'images/rain.png';
+                break;
+            case 'Snow':
+                image.src = 'images/snow.png';
+                break;
+            case 'Clouds':
+                image.src = 'images/cloud.png';
+                break;
+            case 'Mist':
+                image.src = 'images/mist.png';
+                break;
+            case 'Haze':
+                image.src = 'images/mist.png';
+                break;
+            default:
+                image.src = 'images/cloud.png';
+        }
+    });
+});
